83410d2a21dc2f65069c4e40a36a4445b7c4889a
imsarath <sarath.murugan@outlook.com>
2025-02-28 12:26:44 +0100

    update: added COV clinical ovarian project

--- pipeline/utils/clinseq_barcodes.py
+++ pipeline/utils/clinseq_barcodes.py
@@ -277,7 +277,7 @@ def extract_kit_id(kit_string, validation = True):
 
 
 def project_valid(project_str):
-    return project_str in ["AL", "LB", "OT", "PB", "PSFF", "UL", "iPCM", "CRCR", "SARC", "CPC", "BM", "KA", "UM"]
+    return project_str in ["AL", "LB", "OT", "PB", "PSFF", "UL", "iPCM", "CRCR", "SARC", "CPC", "BM", "KA", "UM", "COV"]
 
 
 def sdid_valid(sdid_str):
