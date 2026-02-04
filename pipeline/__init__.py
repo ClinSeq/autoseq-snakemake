7a36cfa71f775e51848716e1be5980c73cbe0f97
imsarath <sarath.murugan@outlook.com>
2026-02-04 13:29:40 +0100

    Bump version: 3.11.2 → 3.11.3

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -3,4 +3,4 @@ Autoseq - pipeline
 
 """
 
-__version__ = '3.11.2'
+__version__ = '3.11.3'
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '3.11.2'
+version = '3.11.3'
 
 setup(name='autoseq',
       version=version,
